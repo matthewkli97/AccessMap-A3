82981e2aa4b224db983b5feaba0a5b8f685a1235
Matthew Li <matthewli@D-10-18-227-148.dhcp4.washington.edu>
2017-11-28 16:13:51 -0800

    upload prototype scripts

--- scripts/Mapping.py
+++ scripts/Mapping.py
@@ -2,20 +2,41 @@
 import geopandas as gpd
 import matplotlib.pyplot as plt
 import pandas as pd
+import sys
+import argparse
+import overpass
 
-%matplotlib inline
+
+#filename = sys.argv[-1]
+
+# observations = gpd.read_file(filename);
+
+# Read in file via command line
 
 #%%
 observations = gpd.read_file('./data/SidewalkObservations/SidewalkObservations.shp')
 
+
+
 #%%
 sidewalks = gpd.read_file('./data/map.geojson')
 
-print(sidewalks)
+
+api = overpass.API()
+response = api.Get('node["name"="Salt Lake City"]')
+
+#%% 
+print(response)
 
 #%%
-lines = sidewalks[sidewalks.geometry.type == 'LineString']
-lines.plot()
+map_query = overpass.MapQuery(50.746,7.154,50.748,7.157)
+response = api.Get(map_query)
 
-#%%
-print(observations)
+# print(sidewalks)
+
+# #%%
+# lines = sidewalks[sidewalks.geometry.type == 'LineString']
+# lines.plot()
+
+# #%%
+# print(observations)
